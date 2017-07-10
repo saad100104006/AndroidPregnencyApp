1ef92a5bbac07e8d1bb44169e66dcf491d3ace67
Md. Tanvir Hossain <saadtan@gmail.com>
2017-07-10 22:11:28 +0600

    test

--- app/src/main/java/info/androidhive/instantapps/saadtest/MainActivity.java
+++ app/src/main/java/info/androidhive/instantapps/saadtest/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //ddd
         
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
